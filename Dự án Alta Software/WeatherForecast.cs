6b3d1363fc09241b23c0181cff0b0403512c3b21
PREDATOR <PREDATOR@DESKTOP-KBK3MKM>
2022-06-02 11:16:59 +0700

    Add project files.

---
+++ Dự án Alta Software/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Dự_án_Alta_Software
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
